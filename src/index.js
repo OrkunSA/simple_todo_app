626c55dc5359a090c44374c52a3173b8f127eb52
Orkun Saglam <c.orkun.s@gmail.com>
2021-11-10 11:29:10 -0700

    create selected list id key for local storage

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const newListForm = document.querySelector('[data-new-list-form');
 const newListInput = document.querySelector('[data-new-list-input');
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
-let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY) || []
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListID';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
 newListForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -12,7 +14,7 @@ newListForm.addEventListener('submit', (e) => {
   const list = createList(listName);
   newListInput.value = null;
   lists.push(list);
-  render();
+  saveAndRender();
 });
 
 function createList(name) {
@@ -22,9 +24,13 @@ function createList(name) {
     tasks: [],
   };
 }
+function saveAndRender() {
+  save();
+  render();
+}
 
 function save() {
-    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
 }
 function render() {
   clearElement(listsContainer);
